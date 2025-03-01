099fa6e588d89d0af88527fa5d4f8d28f3a2ea99
Mohd Saif <106275738+saif0056@users.noreply.github.com>
2025-03-01 10:32:29 +0530

    Create StringIsPallindrome.java

---
+++ StringIsPallindrome.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+       String str = "madam";
+       String reversed = new StringBuilder(str).reverse().toString();
+       if(str.equals(reversed)) {
+             System.out.println(str + " is a palindrome.");
+       }else{
+           System.out.println(str + " is not palindrome.");
+       }
+    }
+}
